cbb90056c448114fa8e71d1ae0874da496dc6267
Peshang Hiwa <51766800+peshanghiwa@users.noreply.github.com>
2022-07-06 01:10:31 +0300

    fixed background color issue in header component

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -77,180 +77,185 @@ const changeCurrency = (currency) => {
 </script>
 
 <template>
-  <header
-    class="bg-white w-full px-3 md:px-0 md:w-[85%] mx-auto h-[60px] flex items-center justify-between sm:border-b-2 md:border-none border-light-grey"
-  >
-    <img
-      src="../../assets/SVGs/logo.svg"
-      class="h-[25px] md:h-[35px]"
-      alt="Cloud Travel Logo"
-    />
-    <!-- Large Screen Only -->
-    <ul class="hidden md:flex gap-x-4 z-50">
-      <li>
-        <button
-          class="px-3 py-1.5 text-black text-lg transition duration-150 ease-in-out flex items-center"
-          type="button"
-          @click="showLanguagesDropdown = !showLanguagesDropdown"
-        >
-          {{ selectedLanguage.name }}
+  <header class="bg-white sm:border-b-2 md:border-none border-light-grey">
+    <div
+      class="w-full px-3 md:px-0 md:w-[85%] mx-auto h-[60px] flex items-center justify-between"
+    >
+      <img
+        src="../../assets/SVGs/logo.svg"
+        class="h-[25px] md:h-[35px]"
+        alt="Cloud Travel Logo"
+      />
+      <!-- Large Screen Only -->
+      <ul class="hidden md:flex gap-x-4 z-50">
+        <li>
+          <button
+            class="px-3 py-1.5 text-black text-lg transition duration-150 ease-in-out flex items-center"
+            type="button"
+            @click="showLanguagesDropdown = !showLanguagesDropdown"
+          >
+            {{ selectedLanguage.name }}
+            <img
+              src="../../assets/SVGs/arrow-down.svg"
+              class="w-2 ml-2"
+              height="30"
+              width="30"
+              alt="Arrow Down"
+            />
+          </button>
+          <ul
+            class="absolute text-base py-1 list-none shadow-lg m-0"
+            v-if="showLanguagesDropdown"
+          >
+            <li
+              v-for="(language, index) in renderedLanguages"
+              :key="index"
+              class="dropdown-item cursor-pointer text-lg py-2 px-4 bg-transparent transition duration-150 text-black bg-white hover:bg-light-grey"
+              @click="changeLanguage(language)"
+            >
+              {{ language.name }}
+            </li>
+          </ul>
+        </li>
+        <li>
+          <button
+            class="px-3 py-1.5 text-black text-lg transition duration-150 ease-in-out flex items-center"
+            type="button"
+            @click="showCurrenciesDropdown = !showCurrenciesDropdown"
+          >
+            {{ selectedCurrency.name }}
+            <img
+              src="../../assets/SVGs/arrow-down.svg"
+              class="w-2 ml-2"
+              height="30"
+              width="30"
+              alt=""
+            />
+          </button>
+          <ul
+            class="absolute text-base py-1 list-none shadow-lg m-0"
+            v-if="showCurrenciesDropdown"
+          >
+            <li
+              v-for="(currency, index) in renderedCurrencies"
+              :key="index"
+              class="dropdown-item cursor-pointer text-lg py-2 px-4 bg-transparent transition duration-150 text-black bg-white hover:bg-light-grey"
+              @click="changeCurrency(currency)"
+            >
+              {{ currency.name }}
+            </li>
+          </ul>
+        </li>
+        <li>
+          <button class="px-3 py-1.5">My Account</button>
+        </li>
+        <li>
+          <button class="px-3 py-1.5">Log Out</button>
+        </li>
+      </ul>
+
+      <!-- Mobile Screen Only -->
+      <div class="block md:hidden">
+        <button @click="showSidebar = !showSidebar">
           <img
-            src="../../assets/SVGs/arrow-down.svg"
-            class="w-2 ml-2"
-            height="30"
-            width="30"
-            alt="Arrow Down"
+            src="../../assets/SVGs/burger-menu.svg"
+            class="h-[18px]"
+            alt="Burger Menu"
+            srcset=""
           />
         </button>
-        <ul
-          class="absolute text-base py-1 list-none shadow-lg m-0"
-          v-if="showLanguagesDropdown"
+        <div
+          class="overflow-y-auto absolute right-2 w-[180px] py-4 px-3 bg-dark-grey rounded shadow-sm z-50"
+          v-if="showSidebar"
         >
-          <li
-            v-for="(language, index) in renderedLanguages"
-            :key="index"
-            class="dropdown-item cursor-pointer text-lg py-2 px-4 bg-transparent transition duration-150 text-black bg-white hover:bg-light-grey"
-            @click="changeLanguage(language)"
-          >
-            {{ language.name }}
-          </li>
-        </ul>
-      </li>
-      <li>
-        <button
-          class="px-3 py-1.5 text-black text-lg transition duration-150 ease-in-out flex items-center"
-          type="button"
-          @click="showCurrenciesDropdown = !showCurrenciesDropdown"
-        >
-          {{ selectedCurrency.name }}
-          <img
-            src="../../assets/SVGs/arrow-down.svg"
-            class="w-2 ml-2"
-            height="30"
-            width="30"
-            alt=""
-          />
-        </button>
-        <ul
-          class="absolute text-base py-1 list-none shadow-lg m-0"
-          v-if="showCurrenciesDropdown"
-        >
-          <li
-            v-for="(currency, index) in renderedCurrencies"
-            :key="index"
-            class="dropdown-item cursor-pointer text-lg py-2 px-4 bg-transparent transition duration-150 text-black bg-white hover:bg-light-grey"
-            @click="changeCurrency(currency)"
-          >
-            {{ currency.name }}
-          </li>
-        </ul>
-      </li>
-      <li>
-        <button class="px-3 py-1.5">My Account</button>
-      </li>
-      <li>
-        <button class="px-3 py-1.5">Log Out</button>
-      </li>
-    </ul>
-
-    <!-- Mobile Screen Only -->
-    <div class="block md:hidden">
-      <button @click="showSidebar = !showSidebar">
-        <img
-          src="../../assets/SVGs/burger-menu.svg"
-          class="h-[18px]"
-          alt="Burger Menu"
-          srcset=""
-        />
-      </button>
-      <div
-        class="overflow-y-auto absolute right-2 w-[180px] py-4 px-3 bg-dark-grey rounded shadow-sm z-50"
-        v-if="showSidebar"
-      >
-        <ul class="space-y-2">
-          <li>
-            <button
-              type="button"
-              class="flex items-center p-2 w-full text-lg text-white rounded-lg transition duration-75 group hover:bg-secondary"
-              @click="showLanguagesDropdown = !showLanguagesDropdown"
-            >
-              <span class="flex-1 text-left whitespace-nowrap">{{
-                selectedLanguage.name
-              }}</span>
-              <svg
-                class="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
+          <ul class="space-y-2">
+            <li>
+              <button
+                type="button"
+                class="flex items-center p-2 w-full text-lg text-white rounded-lg transition duration-75 group hover:bg-secondary"
+                @click="showLanguagesDropdown = !showLanguagesDropdown"
               >
-                <path
-                  fill-rule="evenodd"
-                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </button>
-            <ul v-if="showLanguagesDropdown" class="py-2 space-y-2">
-              <li v-for="(language, index) in renderedLanguages" :key="index">
-                <button
-                  @click="changeLanguage(language)"
-                  class="flex items-center justify-center p-2 w-full text-base text-white rounded-lg transition duration-75 group hover:bg-secondary"
+                <span class="flex-1 text-left whitespace-nowrap">{{
+                  selectedLanguage.name
+                }}</span>
+                <svg
+                  class="w-6 h-6"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  {{ language.name }}
-                </button>
-              </li>
-            </ul>
-          </li>
+                  <path
+                    fill-rule="evenodd"
+                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </button>
+              <ul v-if="showLanguagesDropdown" class="py-2 space-y-2">
+                <li v-for="(language, index) in renderedLanguages" :key="index">
+                  <button
+                    @click="changeLanguage(language)"
+                    class="flex items-center justify-center p-2 w-full text-base text-white rounded-lg transition duration-75 group hover:bg-secondary"
+                  >
+                    {{ language.name }}
+                  </button>
+                </li>
+              </ul>
+            </li>
 
-          <li>
-            <button
-              type="button"
-              class="flex items-center p-2 w-full text-lg text-white rounded-lg transition duration-75 group hover:bg-secondary"
-              @click="showCurrenciesDropdown = !showCurrenciesDropdown"
-            >
-              <span class="flex-1 text-left whitespace-nowrap">{{
-                selectedCurrency.name
-              }}</span>
-              <svg
-                class="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
+            <li>
+              <button
+                type="button"
+                class="flex items-center p-2 w-full text-lg text-white rounded-lg transition duration-75 group hover:bg-secondary"
+                @click="showCurrenciesDropdown = !showCurrenciesDropdown"
               >
-                <path
-                  fill-rule="evenodd"
-                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </button>
-            <ul v-if="showCurrenciesDropdown" class="py-2 space-y-2">
-              <li v-for="(currency, index) in renderedCurrencies" :key="index">
-                <button
-                  @click="changeCurrency(currency)"
-                  class="flex items-center justify-center p-2 w-full text-base text-white rounded-lg transition duration-75 group hover:bg-secondary"
+                <span class="flex-1 text-left whitespace-nowrap">{{
+                  selectedCurrency.name
+                }}</span>
+                <svg
+                  class="w-6 h-6"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  {{ currency.name }}
-                </button>
-              </li>
-            </ul>
-          </li>
+                  <path
+                    fill-rule="evenodd"
+                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </button>
+              <ul v-if="showCurrenciesDropdown" class="py-2 space-y-2">
+                <li
+                  v-for="(currency, index) in renderedCurrencies"
+                  :key="index"
+                >
+                  <button
+                    @click="changeCurrency(currency)"
+                    class="flex items-center justify-center p-2 w-full text-base text-white rounded-lg transition duration-75 group hover:bg-secondary"
+                  >
+                    {{ currency.name }}
+                  </button>
+                </li>
+              </ul>
+            </li>
 
-          <li class="">
-            <button
-              class="flex items-center p-2 text-lg rounded-lg text-white hover:bg-secondary w-full"
-            >
-              My Account
-            </button>
-          </li>
-          <li class="">
-            <button
-              class="flex items-center p-2 text-lg rounded-lg text-white hover:bg-secondary w-full"
-            >
-              Log Out
-            </button>
-          </li>
-        </ul>
+            <li class="">
+              <button
+                class="flex items-center p-2 text-lg rounded-lg text-white hover:bg-secondary w-full"
+              >
+                My Account
+              </button>
+            </li>
+            <li class="">
+              <button
+                class="flex items-center p-2 text-lg rounded-lg text-white hover:bg-secondary w-full"
+              >
+                Log Out
+              </button>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </header>
