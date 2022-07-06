9961faaa4472da090244dc15c0373850d2a0086e
Peshang Hiwa <51766800+peshanghiwa@users.noreply.github.com>
2022-07-07 01:44:24 +0300

    updated checkboxlist component to support v-html

--- src/components/utility/CheckboxList.vue
+++ src/components/utility/CheckboxList.vue
@@ -17,21 +17,27 @@ const { modelValue, name } = toRefs(props);
 </script>
 
 <template>
-  <div v-for="checkbox in modelValue" :key="checkbox.id">
-    <input
-      type="checkbox"
-      :id="`${name}-checkbox-${checkbox.id}`"
-      v-model="checkbox.value"
-      @change="$emit('update:modelValue', modelValue)"
-      class="w-[15px] h-[15px] text-primary bg-white rounded border-dark-grey focus:ring-primary accent-primary"
-    />
+  <div
+    v-for="checkbox in modelValue"
+    :key="checkbox.id"
+    class="flex justify-between mb-2"
+  >
+    <div class="flex items-center">
+      <input
+        type="checkbox"
+        :id="`${name}-checkbox-${checkbox.id}`"
+        v-model="checkbox.value"
+        @change="$emit('update:modelValue', modelValue)"
+        class="w-[15px] h-[15px] text-primary bg-white rounded border-dark-grey focus:ring-primary accent-primary"
+      />
 
-    <label
-      class="text-sm text-dark-grey relative bottom-[3px] left-2"
-      :for="`${name}-checkbox-${checkbox.id}`"
-    >
-      <span v-html="checkbox.title" />
-    </label>
+      <label
+        class="text-sm text-dark-grey relative left-2"
+        :for="`${name}-checkbox-${checkbox.id}`"
+      >
+        <span v-html="checkbox.title" />
+      </label>
+    </div>
 
     <span class="float-right text-medium-grey text-sm">
       {{ checkbox.number }}</span
