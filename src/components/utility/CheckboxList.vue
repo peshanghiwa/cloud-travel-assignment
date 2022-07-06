d365308d6c996bb42e935f7c39333b257094ce4b
Peshang Hiwa <51766800+peshanghiwa@users.noreply.github.com>
2022-07-07 01:00:08 +0300

    created checkbox list generator component

---
+++ src/components/utility/CheckboxList.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { toRefs } from "vue";
+const props = defineProps({
+  modelValue: {
+    type: Array,
+    default: [],
+  },
+});
+
+defineEmits(["update:modelValue"]);
+
+const { modelValue } = toRefs(props);
+</script>
+
+<template>
+  <div v-for="checkbox in modelValue" :key="checkbox.id">
+    <input
+      type="checkbox"
+      :id="`checkbox${checkbox.id}`"
+      v-model="checkbox.value"
+      @change="$emit('update:modelValue', modelValue)"
+      class="w-[15px] h-[15px] text-primary bg-white rounded border-dark-grey focus:ring-primary accent-primary"
+    />
+    <label
+      class="text-sm text-dark-grey relative bottom-[3px] left-2"
+      :for="`checkbox${checkbox.id}`"
+    >
+      {{ checkbox.title }}
+    </label>
+
+    <span class="float-right text-medium-grey text-sm">
+      {{ checkbox.number }}</span
+    >
+  </div>
+</template>
