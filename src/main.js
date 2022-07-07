7bbb292aa82fcdf70dbe3df81df6d19f83231bb8
Peshang Hiwa <51766800+peshanghiwa@users.noreply.github.com>
2022-07-07 22:28:02 +0300

    renamed click-outside directive to clickOutside

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/main.css";
 import "@vueform/slider/themes/default.css";
-import clickOutside from "./directives/click-outside.js";
+import clickOutside from "./directives/clickOutside.js";
 
 const app = createApp(App);
 app.use(clickOutside);
