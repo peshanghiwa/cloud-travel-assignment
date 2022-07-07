210023c9db8fa43937ce583504a5fdf49423a4bc
Peshang Hiwa <51766800+peshanghiwa@users.noreply.github.com>
2022-07-08 01:01:58 +0300

    created mobile version filter dropdown

--- src/components/main/MainContent.vue
+++ src/components/main/MainContent.vue
@@ -1,6 +1,7 @@
 <script setup>
 import sortbyFilterData from "../../composables/sortbyFilter";
 import { reactive, toRefs } from "vue";
+import PropertyCard from "./PropertyCard.vue";
 
 const {
   sortbyOptionsList,
@@ -11,22 +12,100 @@ const {
 
 const data = reactive({
   selectedfilter: 1,
+  filterDropdown: false,
 });
 
-const { selectedfilter } = toRefs(data);
+const { selectedfilter, filterDropdown } = toRefs(data);
+
+const setSelectedFilter = (id) => {
+  selectedfilter.value = id;
+  if (filterDropdown) filterDropdown.value = false;
+};
 </script>
 
 <template>
-  <section class="grow-0 lg:grow">
-    <h1 class="text-lg font-bold">Singapore: 9999 properties found</h1>
-    <section class="flex rounded-md bg-white h-[45px] mt-3 text-base">
+  <section class="grow-0 lg:grow px-4 lg:px-0 mt-5 lg:mt-0">
+    <div
+      class="md:hidden h-[30px] bg-primary flex justify-between items-center rounded px-3 mb-0 cursor-pointer relative"
+    >
+      <div
+        class="w-full h-full absolute top-0 left-0"
+        @click="filterDropdown = !filterDropdown"
+      ></div>
+      <div
+        v-if="filterDropdown"
+        class="absolute rounded overflow-hidden w-full top-[35px] right-[0px] bg-white shadow-xl"
+      >
+        <button
+          class="w-full h-[35px] hover:bg-light-grey flex items-center justify-between px-3"
+          @click="setSelectedFilter(1)"
+        >
+          <span>Popularity</span>
+          <img
+            src="../../assets/SVGs/tick.svg"
+            width="18"
+            :class="{
+              hidden: selectedfilter !== 1,
+            }"
+            alt=""
+          />
+        </button>
+        <button
+          class="w-full h-[35px] hover:bg-light-grey flex items-center justify-between px-3"
+          @click="setSelectedFilter(2)"
+        >
+          <span>Reviews</span>
+          <img
+            src="../../assets/SVGs/tick.svg"
+            width="18"
+            :class="{
+              hidden: selectedfilter !== 2,
+            }"
+            alt=""
+          />
+        </button>
+        <button
+          class="w-full h-[35px] hover:bg-light-grey flex items-center justify-between px-3"
+          @click="setSelectedFilter(3)"
+        >
+          <span>Discount</span>
+          <img
+            src="../../assets/SVGs/tick.svg"
+            width="18"
+            :class="{
+              hidden: selectedfilter !== 3,
+            }"
+            alt=""
+          />
+        </button>
+      </div>
+      <span class="text-white text-base">Select One</span>
+      <svg
+        class="h-[15px] w-[15px]"
+        aria-hidden="true"
+        focusable="false"
+        data-prefix="fas"
+        data-icon="caret-down"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 320 512"
+      >
+        <path
+          fill="white"
+          d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"
+        ></path>
+      </svg>
+    </div>
+    <h1 class="text-lg font-bold mt-4 lg:mt-0">
+      Singapore: 9999 properties found
+    </h1>
+    <section class="hidden md:flex rounded-md bg-white h-[45px] mt-3 text-base">
       <button
         class="w-full h-full border-r-[1px] border-r-light-grey flex justify-center items-center transition-all duration-150 rounded-tl-md rounded-bl-md"
         :class="{
           'bg-primary': selectedfilter === 1,
           'text-white': selectedfilter === 1,
         }"
-        @click="selectedfilter = 1"
+        @click="setSelectedFilter(1)"
       >
         Popularity
       </button>
@@ -60,7 +139,7 @@ const { selectedfilter } = toRefs(data);
           'bg-primary': selectedfilter === 2,
           'text-white': selectedfilter === 2,
         }"
-        @click="selectedfilter = 2"
+        @click="setSelectedFilter(2)"
       >
         Reviews
       </button>
@@ -70,10 +149,13 @@ const { selectedfilter } = toRefs(data);
           'bg-primary': selectedfilter === 3,
           'text-white': selectedfilter === 3,
         }"
-        @click="selectedfilter = 3"
+        @click="setSelectedFilter(3)"
       >
         Discount
       </button>
     </section>
+    <article class="mt-5">
+      <PropertyCard />
+    </article>
   </section>
 </template>
