ab9eaca02bda11286df719825e13142cc2a8662e
Peshang Hiwa <51766800+peshanghiwa@users.noreply.github.com>
2022-07-09 12:43:17 +0300

    created shimmer

---
+++ src/components/main/shimmer.vue
@@ -0,0 +1,46 @@
+<script setup>
+// min-h-[155px] md:min-h-[230px]
+
+import { useWindowSize } from "vue-window-size";
+const { width: windowWidth } = useWindowSize();
+</script>
+
+<template>
+  <section
+    v-for="i in 2"
+    class="w-full min-h-[155px] md:min-h-[230px] rounded-md transition duration-150 p-[10px] flex relative mb-5 z-[1]"
+  >
+    <div
+      class="w-[100px] min-w-[100px] md:w-[200px] md:min-w-[200px] h-auto flex flex-col gap-[2px]"
+    >
+      <free-style-shimmer
+        :is-loading="true"
+        :height="windowWidth < 768 ? '135px' : '210px'"
+        :width="windowWidth < 768 ? '100px' : '200px'"
+        color="#bdbdbd"
+        border-radius="0px"
+      />
+    </div>
+
+    <div class="grow p-3 mb-[60px] md:mb-0">
+      <free-style-shimmer
+        class="mb-3"
+        v-for="i in 3"
+        :is-loading="true"
+        border-radius="0px"
+        width="100%"
+        height="20px"
+        bor
+        color="#bdbdbd"
+      />
+      <free-style-shimmer
+        class="transform translate-y-[30px] hidden md:block"
+        :is-loading="true"
+        border-radius="0px"
+        width="60%"
+        height="20px"
+        color="#bdbdbd"
+      />
+    </div>
+  </section>
+</template>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,14 @@ import "./assets/main.css";
 import "@vueform/slider/themes/default.css";
 import clickOutside from "./directives/clickOutside.js";
 import Popper from "vue3-popper";
+import Shimmer from "vue3-shimmer";
+
+import VueAwesomePaginate from "vue-awesome-paginate";
+import "vue-awesome-paginate/dist/style.css";
 
 const app = createApp(App);
 app.use(clickOutside);
+app.use(VueAwesomePaginate);
+app.use(Shimmer);
 app.component("Popper", Popper);
 app.mount("#app");
